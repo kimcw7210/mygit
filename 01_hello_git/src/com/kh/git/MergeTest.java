3611bdf84d943e0db70aab9d74df141b96c6ff94
kimcw7210 <kimcw7210@gmail.com>
2021-10-02 17:51:51 +0900

    merge base 내용 수정
    x 222 3 4

--- 01_hello_git/src/com/kh/git/MergeTest.java
+++ 01_hello_git/src/com/kh/git/MergeTest.java
@@ -2,8 +2,7 @@ package com.kh.git;
 
 public class MergeTest {
 	public void MergeMe() {
-		 System.out.println(1);
-		 System.out.println(2);
+		 System.out.println(222);
 		 System.out.println(3);
 		 System.out.println(4);
 	}
